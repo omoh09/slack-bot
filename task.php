ba43a948710d81a60ec9989d1fe3b8e5b1d16dcd
Gabriel Ifoga <gabrielifoga@yahoo.com>
2023-08-26 18:24:50 -0400

    feat: Stage 1 task

--- task.php
+++ task.php
@@ -1,15 +1,8 @@
 <?php
 $slack_name = isset($_GET['slack_name']) ? $_GET['slack_name'] : null;
 $track = isset($_GET['track']) ? $_GET['track'] : null;
-
-// Get the current day of the week
 $currentDayOfWeek = date('l');
-
-// Get the current UTC time (with validation of +/-2 hours)
 $currentUTCTime = gmdate('Y-m-d H:i:s');
-
-// Define other information
-$name = "Gabriel";
 $githubFileURL = "https://github.com/omoh09/slack-bot/blob/welcome-bot/task.php";
 $githubSourceURL = "https://github.com/omoh09/slack-bot";
 
