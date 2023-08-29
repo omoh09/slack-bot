ffb0b47e3f2da23a73d99152f578e69d425ef10f
Gabriel Ifoga <gabrielifoga@yahoo.com>
2023-08-29 15:32:01 +0100

    Create task.php

---
+++ api/task.php
@@ -0,0 +1,24 @@
+<?php
+// $slack_name = isset($_GET['slack_name']) ? $_GET['slack_name'] : null;
+// $track = isset($_GET['track']) ? $_GET['track'] : null;
+$slack_name = $argv[1];
+$track = $argv[2];
+$currentDayOfWeek = date('l');
+$currentUTCTime = gmdate('Y-m-d H:i:s');
+$githubFileURL = "https://github.com/omoh09/slack-bot/blob/welcome-bot/task.php";
+$githubSourceURL = "https://github.com/omoh09/slack-bot";
+
+// Prepare the response array
+$response = [
+    'slack_name' => '@'.$slack_name,
+    'current_day' => $currentDayOfWeek,
+    'utc_time' => $currentUTCTime,
+    'track' => $track,
+    'github_file_url' => $githubFileURL,
+    'github_repo_url' => $githubSourceURL,
+];
+
+// Return the response as JSON
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
